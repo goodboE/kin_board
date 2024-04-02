c87f7f3645530f1893b55deb33fa2f2102ac5049
ko <iamkokoko123@gmail.com>
2024-04-02 18:32:28 +0900

    initial commit

---
+++ src/main/java/com/ko/kin/KinApplication.java
@@ -0,0 +1,13 @@
+package com.ko.kin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ko/kin/KinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ko.kin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
